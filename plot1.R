8aeba16e1d308b1da996f0f911a77e08808d9879
Ron Ammar <ron.ammar@gmail.com>
2016-01-10 21:35:42 -0500

    plot1.R outputs correct png

--- plot1.R
+++ plot1.R
@@ -17,7 +17,9 @@ hpc <- hpc %>%
   filter(asDate >= dStart & asDate <= dEnd)
 
 # Plot histogram (plot 1)
+png("plot1.png", width=480, height=480, units=)
 hist(hpc$Global_active_power,
      col="red",
      xlab="Global Active Power (kilowatts)",
      main="Global Active Power")
+dev.off()
